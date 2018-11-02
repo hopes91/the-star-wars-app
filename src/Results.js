cf631b3d96c617b9f3111dc8805f05a755c8c3bd
hopes91 <nadya.me91@gmail.com>
2018-11-02 13:25:34 +0400

    replace the class with an arrow function

--- src/Results.js
+++ src/Results.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Results.css';
 
-class Results extends Component {
-	render() {
-		const RESULTS = this.props.results.map((request, ind) => {
-			let value = request.map((val, ind) => {
-				let twoVal = val.split(': ');
+const Results = (props) => {
+	const RESULTS = props.results.map((request, ind) => {
+		let value = request.map((val, ind) => {
+			let twoVal = val.split(': ');
 
-				return <p key={ind}><span className='bold-value'>{twoVal[0]}: </span>{twoVal[1]}<br /></p>;
-			});
-
-			return <div className='request' key={ind}>{value}<br /></div>;
+			return <p key={ind}><span className='bold-value'>{twoVal[0]}: </span>{twoVal[1]}<br /></p>;
 		});
 
-		return (
-			<div className='app-results'>
-				{RESULTS}
-			</div>
-		);
-	}
+		return <div className='request' key={ind}>{value}<br /></div>;
+	});
+
+	return (
+		<div className='app-results'>
+			{RESULTS}
+		</div>
+	);
 }
 
 export default Results;
